1209cb41d33ba11f2c70db6d6bd53a8d727adbba
B122250 <B122250>
2021-11-07 19:36:31 +0000

    issue solved, this script now downloads and saves all protein sequences for the query

--- Assignment2/proteins.py
+++ Assignment2/proteins.py
@@ -7,4 +7,15 @@ prot_fam = input("Please enter the desired protein family:\n\t")
 print('Fetching Accession values for {} proteins in {}'.format(prot_fam,tax_grp))
 print('Please quit now if these is not the desired query')
 os.system('esearch -db protein -query "' + prot_fam + ' AND ' + tax_grp + '"| efetch -format acc > protein_sequences/acc_numbers.txt')
+#read the accession numbers to get the sequences, and print a warning if longer than 1000
+#probs should add a warning to see if you want to continue
+acc_list=[]
+for line in open("protein_sequences/acc_numbers.txt", "r"):
+	acc_list.append(line.strip())
+if (len(acc_list) > 1000):
+    if input('There are more than 1000 sequences in this analysis, press y if you want to continue ') != 'y':
+        sys.exit()
+print('Fetching Protein Sequences...')
+for acc in acc_list:
+	os.system('wget -q -O protein_sequences/' + acc + '.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id="' + acc + '"&strand=1&rettype=fasta&retmode=text" > protein_sequences/' + acc + '.txt')
 
