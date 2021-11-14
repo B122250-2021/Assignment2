7860937fa62fe96c41fb416a877e14786bae5c11
B122250 <B122250>
2021-11-14 17:09:35 +0000

    added optional arguments, and a small help section. Still needs to be worked on

--- Assignment2/proteins.py
+++ Assignment2/proteins.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python3
 import os,subprocess,shutil,sys,re,glob,ete3
 import numpy as np
 import pandas as pd
 from ete3 import Tree
+import argparse
 
 #input test
 os.system("mkdir protein_sequences && mkdir protein_sequences/individual_sequences && mkdir prosite_motifs")
-tax_grp = input("Please enter the taxonomic group you want to investigate:\n\t")
-prot_fam = input("Please enter the desired protein family:\n\t")
+
+parser=argparse.ArgumentParser(
+    description='''proteins.py performs an analysis based on user input.\nThe user can provide a Taxonomic Group and a Protein Family of their choice.''',
+    epilog="""Please message <my.email> to report any bugs.""")
+parser.add_argument('--taxonomic_group','-t', type=str, help='Taxonomic group of your choice ')
+parser.add_argument('--protein_family', '-p', type=str, help='Protein family of your choice')
+
+args=parser.parse_args()
+prot_fam = args.protein_family 
+tax_grp = args.taxonomic_group
+
+#If no arguments are provided
+if tax_grp is None:
+	tax_grp = input("No taxonomic group was selected. Please enter the taxonomic group you want to investigate:\n\t")
+if prot_fam is None:
+	prot_fam = input("No protein family was selected. Please enter the desired protein family:\n\t")
+
 #Doing an esearch, and piping it to efetch
 
 #need a prompt asking if you want partial or not
@@ -15,7 +30,7 @@ print('Fetching Accession values for {} proteins in {}...'.format(prot_fam,tax_g
 print('Please quit now if these is not the desired query')
 os.system('esearch -db protein -query "' + tax_grp + '[Organism] AND ' + prot_fam + '[Protein] NOT PARTIAL"| efetch -format acc > protein_sequences/acc_numbers.txt')
 #read the accession numbers to get the sequences, and print a warning if longer than 1000
-#Ask the user if they want to seta limit
+#Ask the user if they want to set a limit
 #efetch limit
 acc_list=[]
 for line in open("protein_sequences/acc_numbers.txt", "r"):
@@ -25,8 +40,7 @@ if len(acc_list) > 1000:
 	if input('There are more than 1000 sequences in this analysis, press y if you want to continue ') != 'y':
 		print('Exiting analysis')
 		sys.exit()
-print('Fetching Protein Sequences...')
-os.system('esearch -db protein -query "' + tax_grp + '[Organism] AND ' + prot_fam + '[Protein] NOT PARTIAL" | efetch -format fasta > protein_sequences/input.fa')
+
 print('Done, Counting Species...')
 #Need to figure out how many species I have
 os.system('esearch -db protein -query "' + tax_grp + '[Organism] AND ' + prot_fam + '[Protein] NOT PARTIAL" | efetch -format gbc | xtract -pattern INSDSeq -group INSDFeature -block INSDQualifier -element INSDQualifier_name INSDQualifier_value | grep {} | cut -f2 >  all_species.txt'.format('organism')) #used the .format to avoid sysntax errors with apostrophes
@@ -37,12 +51,8 @@ non_redundant_species=set(species)
 if input('There are {} species in your analysis, you can see them in all_species.txt. Press y if you wish to continue.\n\t'.format(len(non_redundant_species))) != 'y':
 	print('Exiting analysis')
 	sys.exit()
-#I think I need to use BLAST and Clustalo as te tests before continuing to the next section
-#use clustalo to align
-print('Aligning sequences + building phylogenetic tree...')
-os.system('clustalo --threads=64 -i protein_sequences/input.fa -o protein_sequences/aligned_sequences.fa --guidetree-out=clustalo_tree')
-#Making tree file
-#create a dict with acc vs species, to make more usefull tree
+
+#create a dict with acc vs species, useful later on when building tree
 #since the values are written to the file in the order that they are retrieved, acc and species name will be in order in their respective files
 spp_list=[]
 acc_spp = {}
@@ -50,6 +60,18 @@ for line in open("all_species.txt", "r"):
 	spp_list.append(line.strip())
 for a,s in zip(acc_list,spp_list):
 	acc_spp[a] = s
+
+
+print('Fetching Protein Sequences...')
+os.system('esearch -db protein -query "' + tax_grp + '[Organism] AND ' + prot_fam + '[Protein] NOT PARTIAL" | efetch -format fasta > protein_sequences/input.fa')
+
+#I think I need to use BLAST and Clustalo as te tests before continuing to the next section
+#use clustalo to align
+print('Aligning sequences + building phylogenetic tree...')
+os.system('clustalo --threads=64 -i protein_sequences/input.fa -o protein_sequences/aligned_sequences.fa --guidetree-out=clustalo_tree')
+
+
+#Making tree file
 #Now I need to change all acc values in the tree file for the format acc_spp, as a tree that only has the accession value is not very informative
 newtree = open("newick_tree.txt", "a")
 for line in open("clustalo_tree", "r"):
@@ -68,6 +90,7 @@ sys.stdout.close()
 #reopening stdout
 sys.stdout = open("/dev/stdout", "w")
 print('Done. File tree.nw contains Guide tree written in Newick format, tree.txt contains a visual representation of the infered tree.\n Producing conservation plot. Graph will appear on screen, but will also be saved as plotcon.svg')
+
 os.system('plotcon -sequences protein_sequences/aligned_sequences.fa -winsize 4 -graph x11 -graph svg && plotcon -sequences protein_sequences/aligned_sequences.fa -winsize 4 -graph x11')
 print('Done')
 print('Searching for known protein motifs in our query sequences...')
