e0374c60f2d5aab64f55b2634a472f4cb5589d95
B122250 <B122250>
2021-11-22 13:39:23 +0000

    Added notes, script is finished, still need to find a nice way to ask the user id they want to install libraries

--- Assignment2/proteins.py
+++ Assignment2/proteins.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 
-#Using pullseq
-#Ask the user to select from the options:
-#-use all species -use sequences most common species(if so, how many) - use sequences from least common -cycle one by one and decide if you use them
-
+#Importing all the libraries I might use
 import os,subprocess,shutil,sys,re,glob,ete3,collections,argparse,fileinput
 import numpy as np
 import pandas as pd
 from ete3 import Tree
 from collections import Counter
 
+#Adding arguments and a small description that is displayed if the user types -h
 parser=argparse.ArgumentParser(
     description='''proteins.py performs an analysis based on user input.\nThe user can provide a Taxonomic Group and a Protein Family of their choice.''',
     epilog="""Please message <my.email> to report any bugs.""")
-parser.add_argument('--taxonomic_group','-t', type=str, help='Taxonomic group of your choice ')
-parser.add_argument('--protein_family', '-p', type=str, help='Protein family of your choice')
-parser.add_argument('--dir', '-d', type=str, help='Directory where program will be executed')
-parser.add_argument('--winsize', '-w', type=str, help='Window size of conservation plot and hydropathy plot')
-parser.add_argument('--partial', '-partial', type=str, help='y if you want to include partial sequences, anything else will exclude them')
+parser.add_argument('--taxonomic_group','-t', type=str, help='Taxonomic group of your choice ')#Can be specified with --taxonomic_group, or -t
+parser.add_argument('--protein_family', '-p', type=str, help='Protein family of your choice')#Can be specified with --protein_familt, or -p
+parser.add_argument('--dir', '-d', type=str, help='Directory where program will be executed')#Can be specified with --dir, or -d
+parser.add_argument('--winsize', '-w', type=str, help='Window size of conservation plot and hydropathy plot')#Can be specified with --winsize, or -w
+parser.add_argument('--partial', '-partial', type=str, help='y if you want to include partial sequences, anything else will exclude them')#Can be specified with --partial, or -partial
 
-args=parser.parse_args()
+args=parser.parse_args() # Parsing the arguments and assigning them to variables
 prot_fam = args.protein_family 
 tax_grp = args.taxonomic_group
 wd = args.dir
 winsize = args.winsize
 partial = args.partial
 
-#If no arguments are provided
+#If no arguments are provided, the user is asked to enter information that is then assigned to the variables
 if wd is None:
 	wd = input('Your current working directory is {}, please type the path where you want this analysis made:\n(the default is the current working directory, you can just press enter to stay in the current directory):\n\t'.format(os.getcwd()))
 if tax_grp is None:
@@ -39,7 +37,7 @@ if partial == 'y':
 	partial = ''
 else:
 	partial = ' NOT PARTIAL'
-
+#Changing directory to the one that was specified, with error traps that default to the current dir
 try:
 	os.chdir(wd)
 except FileNotFoundError:
@@ -48,29 +46,28 @@ except NotADirectoryError:
 	print("{} is not a directory, defaulting to current directory".format(wd))
 os.system("mkdir protein_sequences && mkdir protein_sequences/individual_sequences && mkdir prosite_motifs reports")
 
-#need a prompt asking if you want partial or not
 print('Fetching Accession values for {} proteins in {}...'.format(prot_fam,tax_grp))
 print('Please quit now if this is not the desired query')
-#read the accession numbers to get the sequences, and print a warning if longer than 1000
-#Ask the user if they want to set a limit
-#efetch limit
+
+#Downloading species name and acc values, making a dictionary using the accession value as the key
 os.system('esearch -db protein -query "' + tax_grp + '[Organism] AND ' + prot_fam + '[Protein]' + partial + '" | efetch -format gbc | xtract -pattern INSDSeq -element INSDSeq_accession-version -element INSDSeq_organism | cut -f1,2 >  acc_spp.txt')
 acc_list=[]
 species=[]
 acc_spp = {}
-for line in open("acc_spp.txt", "r"):
+for line in open("acc_spp.txt", "r"):#Tab sepparated between acc and spp
 	acc_list.append(line.split('\t')[0])
-	species.append(line.split('\t')[1].strip('\n'))
+	species.append(line.split('\t')[1].strip('\n'))#Getting rid of the newline character
 	acc_spp[line.split('\t')[0]] = line.split('\t')[1].strip('\n')
 non_redundant_species = set(species)
 print('There are {} protein sequences, and {} species that match this query.'.format(len(acc_list),len(non_redundant_species)))
 
-
+#read the accession numbers to get the sequences, and print a warning if longer than 1000, or if there are 0 sequences, or 1 sequence
+# I have decided to not add a limit to the esearch, as I don't think that it is needed, given that I give the user the option of filtering sequences later
 if len(acc_list) == 0:
 	print('There are no protein sequences that match your query. Please restart with different input arguments.')
 	print('Exiting analysis')
 	sys.exit()
-elif len(acc_list) ==1:
+elif len(acc_list) == 1:
 	print('There is only one protein that matches your query, this program can\'t use a single sequence as input. Please restart with different input arguments.')
 	print('Exiting analysis')
 	sys.exit()
@@ -78,25 +75,27 @@ if len(acc_list) > 1000:
 	if input('There are more than 1000 sequences in this analysis, press y if you want to continue ') != 'y':
 		print('Exiting analysis')
 		sys.exit()
-
+#I can now download the sequences
 print('Fetching Protein Sequences...')
 os.system('esearch -db protein -query "' + tax_grp + '[Organism] AND ' + prot_fam + '[Protein]' + partial + '" | efetch -format fasta > protein_sequences/all_sequences.fa')
-
-#changing the format of the files that start with sp|, or tr|, as pullseq will just skip them, I encountered these two, but I am aware that there might be more
-#which is why I added the next section which makes sure about the format and prints a warning if unrecognised
 
 #Check the formats of the fasta file, to make sure that they will be analysed by pullseq. At this point, any sequence that doesn't start with the accession value will not be recognised by pullseq
 
 for line in open("protein_sequences/all_sequences.fa", "r"):
 	if line.startswith('>'):
-		if any(acc in line for acc in acc_list) != True:
+		if any(acc in line for acc in acc_list) != True: #If the accession value is not in the header of the sequence
 			print('WARNING! Unrecognised accession value\nThe following sequence:\n{}\nis either not in fasta format, or is in a format that is not supported in this program\n'.format(line))
 
+#Asking the user if they want the most/least common species, all of them, or a pick and choose approach
+#I then select the accession values of those sequences and add them to a pullseq loop
+#By doing the pullseq -g in a loop, I can also use the (annoying) sequences that start with the db name like sp|
 choice = input('\nDo you want to filter your sequences? Please select one of the following options, or press any other key to exit:\n\n\t-Use all sequences for the analysis (Will take a long time for large datasets) (Press 1)\n\n\t-Use sequences from the most common species (Press 2)\n\n\t-Use sequences from the least common species (Press 3)\n\n\t-Cycle one by one and chose which species you want (Not recommended for large datasets) (Press 4)\n\n\t')
+#all sequences
 if choice == '1' :
 	print('All sequences will be used for the analysis. \nThe alignment may take a while, depending on the ammount of sequences')
 	os.system('cp protein_sequences/all_sequences.fa protein_sequences/input.fa ')
 
+#print the most common species by using the argument Counter
 elif choice == '2' :
 	spp_no = int(input('These are the 10 most common species, please select how many species you want to use:\n\t{}\n'.format(Counter(species).most_common(10))))
 	if spp_no > 10:
@@ -106,18 +105,17 @@ elif choice == '2' :
 	selected_species = []
 	numbers = np.linspace(1,spp_no,spp_no)
 	for x in numbers:
-		selected_species.append(Counter(species).most_common(10)[int(x)-1][0])
+		selected_species.append(Counter(species).most_common(10)[int(x)-1][0]) #x-1 is the position of the [species, freq] in the list, and [0] is for the spp name
 	
-	filtered_acc = []
+	filtered_acc = [] #Going back to the acc_spp dicctionary to extract the accession values of the chosen species
 	for species in selected_species:
 		keys = [key for key,value in acc_spp.items() if value == species]
 		for key in keys:
 			filtered_acc.append(key)
-	
-	
+	#pullseq loop to extract the selected species
 	for acc in filtered_acc:
 		os.system('/localdisk/data/BPSM/Assignment2/pullseq -i protein_sequences/all_sequences.fa -g {} >> protein_sequences/input.fa'.format(acc))
-
+#Same approach as above, only with the least common species
 elif choice == '3':
 	spp_no = int(input('These are the 10 least common species, please select how many species you want to use:\n\t{}\n'.format(Counter(species).most_common()[-11:-1])))
 	if spp_no > 10:
@@ -136,31 +134,32 @@ elif choice == '3':
 	
 	for acc in filtered_acc:
 		os.system('/localdisk/data/BPSM/Assignment2/pullseq -i protein_sequences/all_sequences.fa -g {} >> protein_sequences/input.fa'.format(acc))
+#Pick and choose approach
 elif choice == '4':
 	for current_species in non_redundant_species:
 		if input('There are {} occurrances of {} in this dataset. Do you want to include the sequences in the analysis?\n Press y if yes, any other key will discard the sequence(s)'.format(species.count(current_species),current_species)) =='y':
-			keys = [key for key,value in acc_spp.items() if value == current_species]
+			keys = [key for key,value in acc_spp.items() if value == current_species] #If answer is yes, chose the current species
 			for key in keys:
 				os.system('/localdisk/data/BPSM/Assignment2/pullseq -i protein_sequences/all_sequences.fa -g {} >> protein_sequences/input.fa'.format(key))
-
+#If user selects another option...
 else:
 	print('That wasn\'t one of the options...')
 	print('Exiting analysis')
 	sys.exit()
 
-#use clustalo to align
+#use clustalo to align, and downloading clustalo's infered tree, in this case it is a neighbor-joining tree based on the alignment
 print('Aligning sequences + building phylogenetic tree...')
-os.system('clustalo --threads=10 -i protein_sequences/input.fa -o protein_sequences/aligned_sequences.fa --guidetree-out=clustalo_tree')
+os.system('clustalo --threads=10 -i protein_sequences/input.fa -o protein_sequences/aligned_sequences.fa --guidetree-out=clustalo_tree') # I don't think the --threads is helping much
 
 
-#Making tree file
+#Making tree file, everyone likes a tree
 #Now I need to change all acc values in the tree file for the format acc_spp, as a tree that only has the accession value is not very informative
 newtree = open("newick_tree.txt", "a")
 for line in open("clustalo_tree", "r"):
 	if line.startswith('(') or line.startswith(')') or line.startswith(',') or line.startswith(';'):
 		newtree.write(line)
 	elif line.startswith('sp|'):
-		acc = line.split('|')[1] #some sequences are found in this format
+		acc = line.split('|')[1] #many sequences are found in this format, if a sequence is downloaded with another format, such as tr, it will work, but the whole header will be there, instead of the acc and spp
 		spp = acc_spp.get(acc)
 		spp = spp.replace(' ','_')
 		newtree.write(acc + '_' + spp +':' + line.split(':')[1])
@@ -170,13 +169,14 @@ for line in open("clustalo_tree", "r"):
 		spp = spp.replace(' ','_')
 		newtree.write(acc + '_' + spp +':' + line.split(':')[1]) 
 newtree.close()
-sys.stdout = open("tree.txt", "w")
-t = Tree("newick_tree.txt", format=1)
+sys.stdout = open("tree.txt", "w") #tree is printed to stdout, so I send it to a file
+t = Tree("newick_tree.txt", format=1) #same version as the clustalo one, but I have formatted the seq labels to acc_spp
 print(t)
 sys.stdout.close()
-#reopening stdout
+#reopening stdout, I need it to print messages
 sys.stdout = open("/dev/stdout", "w")
-print('Done. File tree.nw contains Guide tree written in Newick format, tree.txt contains a visual representation of the infered tree.') 
+#If winsize was not specified as argument, and adding warnings for large and small winsizes
+print('Done. File newick_tree.txt contains Guide tree written in Newick format.\nThis tree was generated from the alignment, using a neighbor-joining approach.\n tree.txt contains a visual representation of the infered tree.') 
 if winsize is None:
 	winsize = input('Please specify a window size for the conservation plot (4 is recommended). \nThe same window size will be used if you decide to perform more analysis:\n\t')
 if winsize.isnumeric() == False:
@@ -188,18 +188,19 @@ if int(winsize) < 4:
 	print('WARNING:\nThis window size is low, conservation plot might be noisy')
 
 print('Producing conservation plot. Graph will appear on screen, but will also be saved as plotcon.svg')
+#plotcon for svg and for screen output
 os.system('plotcon -sequences protein_sequences/aligned_sequences.fa -winsize {} -graph x11 -graph svg && plotcon -sequences protein_sequences/aligned_sequences.fa -winsize {} -graph x11'.format(winsize,winsize))
 print('Done')
 print('Searching for known protein motifs in the query sequences...')
-#to scan for motifs, file needs to be split
 
+#to scan for motifs, file needs to be split into individual sequences
 inputseq = open("protein_sequences/input.fa", "r")
 content = inputseq.read()
 allsequences = content.split('>')
 inputseq.close()
 for sequence in allsequences[1:]:
 	acc = sequence.split()[0] #retrieve the accession value
-	if acc.startswith('sp'):
+	if acc.startswith('sp') or acc.startswith('tr'): # Again, annoying sequences that start with sp| or tr|. The analysis will work with other db| formats, but the patmatmotif file will have a wierd name
 		acc = acc.split('|')[1]
 	current_file = open('protein_sequences/individual_sequences/' + acc + '.fa','w')
 	current_file.write('>' + sequence)
@@ -220,7 +221,7 @@ for file in files:
 		for line in f:
 			pattern1 = 'Sequence'
 			if re.search( pattern1, line):
-				elements.append(file.split('/')[1][:-16]) #the accession value
+				elements.append(file.split('/')[1][:-16]) #the accession value, will be wrong if starts with sp| etc. Patmatmotifs can't deal with fasta headers in this format, acc name will be wrong
 			pattern2 = 'Motif'
 			if re.search(pattern2,line):
 				elements.append(line.split()[2]) # the motif
@@ -242,19 +243,19 @@ if input('Would you like to calculate the hydrophobic moment for the chosen prot
 if input('Would you like to plot a superimposed hydropathy plot for the chosen sequences (not recommended for large datasets)? If yes, press y\n\t') == 'y':
 	os.system('pepwindowall -sequences protein_sequences/aligned_sequences.fa -window {} -gxtitle="Residue Position" -gytitle="Hydropathy" -graph svg && pepwindowall -sequences protein_sequences/aligned_sequences.fa -window {} -gxtitle="Residue Position" -gytitle="Hydropathy" -graph x11 '.format(winsize,winsize))
 
-#Summarising all the results
+#Summarising all the results, with dictionaries
 acc_motif = {}
 for line in open("reports/motifs.txt", "r"):
 	line = line.strip() # remove newline
 	motifs = ','.join(line.split('\t')[1:]) #Joining all the motifd that were found into comma-separated string
 	acc_motif[line.split('\t')[0]] = motifs
 
-
+#And making a dataframe with the results, columns will be empty if tests were not performed
 df = pd.read_csv("acc_spp.txt", sep='\t', names=['Accession_number','Species','Motifs','Isoelectric_point','Max_Hmoment'])
 for index, row in df.iterrows():
 	df.loc[index,'Motifs'] = acc_motif[df.loc[index,'Accession_number']]
 
-
+#Making a dict with accession, iep so that I can add it to the df at the correct places
 try:
 	acc_iep = {}
 	for line in open("reports/report.iep","r"):
@@ -265,12 +266,13 @@ try:
 			acc_iep[acc] = iep
 	for index, row in df.iterrows():
 		try:
-			df.loc[index,'Isoelectric_point'] = acc_iep[df.loc[index,'Accession_number']]
+			df.loc[index,'Isoelectric_point'] = acc_iep[df.loc[index,'Accession_number']] # for every row, I find the iep in the dict by using tht same rows acc as key
 		except KeyError:
-			df.loc[index,'Isoelectric_point'] = 'NaN'
+			df.loc[index,'Isoelectric_point'] = 'NaN' #For some reason emboss programs mess up the sequence id when the fasta format is from 
 except FileNotFoundError:
 		print('Isoelectric point was not calculated, skipping from final report')
-
+		df['Isoelectric_point'] = 'NaN'
+#same concept as before, this time with the hmoment instead
 try:
 	acc_hmoment = {}
 	for line in open('reports/report.hmoment','r'):
@@ -287,5 +289,8 @@ try:
 
 except FileNotFoundError:
 		print('Hydrophobic moment was not calculated, skipping from final report')
-
+		df['Max_Hmoment'] = 'NaN'
+#Save the df
 df.to_csv('output.tsv',sep='\t')
+#Remove unwanted files
+os.system('rm -fr acc_spp.txt  clustalo_tree')
